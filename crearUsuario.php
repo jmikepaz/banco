b7ea660348ceb9faa916da7633d6d6b6f787d122
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-07-29 21:46:05 -0600

    headers

--- crearUsuario.php
+++ crearUsuario.php
@@ -1,7 +1,7 @@
 <?php
  header('Access-Control-Allow-Origin: *');
- header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
- header('Access-Control-Allow-Headers: token, Content-Type');
+ header('Access-Control-Allow-Methods: *');
+ header('Access-Control-Allow-Headers: *');
  header('Access-Control-Max-Age: 1728000');
  header('Content-Length: 0'); 
  header('Content-Type: application/json');
