041806eb736a2905a0f42439d0dedde3c6e4a35f
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-07-29 21:43:10 -0600

    cors

--- crearUsuario.php
+++ crearUsuario.php
@@ -1,4 +1,10 @@
 <?php
+ header('Access-Control-Allow-Origin: *');
+ header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+ header('Access-Control-Allow-Headers: token, Content-Type');
+ header('Access-Control-Max-Age: 1728000');
+ header('Content-Length: 0'); 
+ header('Content-Type: application/json');
 include "./conexion.php";
 $postdata = file_get_contents("php://input"); 
 
