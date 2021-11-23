3c8f8e63959e4dcf30132ec2b2ad90892d60d499
Juan Paz <juan.paz@grupoemco.com>
2021-11-23 14:20:54 -0600

    pdf

--- controller/core/recoveryPassword.js
+++ controller/core/recoveryPassword.js
@@ -1,5 +1,12 @@
 const pg = require('../../config/conection')
-const sendmail = require('../email/email')
+const sendmail = require('../email/email')  
+var fs = require("fs"); 
+//var pdf = require('html-pdf');
+var pdf = require("pdf-creator-node");
+//var html = fs.readFileSync('./test/businesscard.html', 'utf8');
+var options = { format: 'Letter' };
+var html = fs.readFileSync("views/welcome.handlebars", "utf8"); 
+
 
 exports.recoveryPassword = async function(req, res, next) {  
   let recovery = await pg.func('public.ft_proc_recovery_account', [JSON.stringify(req.body)]).catch(err => {
@@ -7,10 +14,34 @@ exports.recoveryPassword = async function(req, res, next) {
   })   
   recovery = recovery[0]["ft_proc_recovery_account"]
   try {
+    // Read HTML Template
+   
     if (recovery) {
-      if (recovery.estado) {
-        sendmail.sendEmailRecoveryPassword(recovery.nombre , recovery.email, recovery.usuario, recovery.password)
-      }
+      // pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+      //   if (err) return console.log(err);
+      //   console.log(res); // { filename: '/app/businesscard.pdf' }
+      // });
+
+      var document = {
+        html: html,
+        data: {
+          users: recovery,
+        },
+        path: "./output.pdf",
+        type: "",
+      };
+
+      pdf.create(document, options)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      //if (recovery.estado) {
+       // sendmail.sendEmailRecoveryPassword(recovery.nombre , recovery.email, recovery.usuario, recovery.password)
+      //}
       res.send(recovery)
     }
   } catch (error) {
