f662c2f052a740d8433472e63964b104c7ab4b81
Juan Paz <juan.paz@grupoemco.com>
2021-11-30 16:46:46 -0600

    ramdom

--- controller/core/sendCodeVerification.js
+++ controller/core/sendCodeVerification.js
@@ -6,10 +6,9 @@ var pdf = require("pdf-creator-node");
 //var html = fs.readFileSync('./test/businesscard.html', 'utf8');
 var options = { format: 'Letter' };
 var html = fs.readFileSync("views/welcome.handlebars", "utf8"); 
-const crypto = require("crypto");
-
+ 
 exports.sendCodeVerification = async function(req, res, next) {  
-  const number = crypto.randomInt(0, 10000);
+  var number = getRandomInt(1990, 9999) 
   const verificationCode = Math.round( number.toString().padStart(4, "0") );
  
   try { 
@@ -19,5 +18,8 @@ exports.sendCodeVerification = async function(req, res, next) {
   } catch (error) {
      res.send({error:true , mesage:error})
   }
- 
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 }
