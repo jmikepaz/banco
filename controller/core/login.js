93fec477899de55b343e11e6cbb1db0337fa3b25
Juan Paz <juan.paz@grupoemco.com>
2021-12-09 11:28:15 -0600

    login

--- controller/core/login.js
+++ controller/core/login.js
@@ -1,12 +1,17 @@
 const pg = require('../../config/conection')
 
 exports.login = async function(req, res, next) {  
+  console.log(req.body);
   let login = await pg.func('public.ft_get_login', [JSON.stringify(req.body)]).catch(err => {
     console.log(err)
   })   
   login = login[0]["ft_get_login"]
   try {
     if (login) {
+
+      console.log('*****LOGIN*****');
+      console.log(login);
+      console.log('*****LOGIN*****');
       res.send(login)
     }
   } catch (error) {
